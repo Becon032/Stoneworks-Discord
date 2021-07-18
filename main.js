478ba3479aec99bcb9212b80af2da941cbf7c6f2
Becon032 <bartoszjarzebowski032@gmail.com>
2021-07-18 16:10:08 +0200

    Added command handler

--- main.js
+++ main.js
@@ -2,26 +2,40 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const prefix = '!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 
 client.once('ready', () => {
     console.log('im alive!');
 });
 
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    
+    const args = message.content.slice(prefix.length).split(/ + /);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    }
+
+
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.login('ODY2MzEwMDg1MzMyNDM0OTQ0.YPQsIw.7Tz1ZEDw_j8tjcV4EZmQc1AFO8c');
+client.login('ODY2MzEwMDg1MzMyNDM0OTQ0.YPQsIw.g5V4dzY-vo8lYegIyh0EbqQrJwE');
