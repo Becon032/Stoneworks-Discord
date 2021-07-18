673626c3197fa02ac849a1fc15712ac5aa799fe8
Becon032 <bartoszjarzebowski032@gmail.com>
2021-07-18 15:38:38 +0200

    Added discord library

---
+++ main.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('im alive!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('ODY2MzEwMDg1MzMyNDM0OTQ0.YPQsIw.7Tz1ZEDw_j8tjcV4EZmQc1AFO8c');
