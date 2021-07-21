e63b43795289ead25b9c5a9c41bd7cb8a2b2a72e
Becon032 <bartoszjarzebowski032@gmail.com>
2021-07-21 02:56:59 +0200

    millitary command fixed

--- commands/military.js
+++ commands/military.js
@@ -1,12 +1,12 @@
 module.exports = {
     name: 'military',
     description: "military reaction roles",
-    async execute(client) {
+    async execute(Discord, client) {
 
         const selectChannel = '866751526277218304';
-        const recruitChannel ='866784449853259776';
+        const botCommunicationChannel ='866784449853259776';
 
-        const recruit = '866784910147977216';
+        const applicant = '866784910147977216';
 
         const armyEmoji = '⚔️';
         const dragoonEmoji = '🏇';
@@ -18,54 +18,47 @@ module.exports = {
             if(user.bot) return;
             if(!reaction.message.guild) return;
 
-            if(reaction.message.channel.id == selectChannel){
-                switch(reaction.emoji.name){
-                    case armyEmoji:
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(recruit);
-                        client.channels.cache.get(recruitChannel).send('there is a new army recruit named :');
-                        break;
-                    case dragoonEmoji:
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(recruit);
-                        client.channels.cache.get(recruitChannel).send('there is a new dragoon recruit named :');
-                        break;
-                    case guardEmoji:
-                        await reaction.message.guild.members.cache.get(user.id).roles.add(recruit);
-                        client.channels.cache.get(recruitChannel).send('there is a new guard recruit named :');
-                        break;
-                    default:
-                        console.error("there is no emoji like that in jobs");
-                }
+            if(reaction.message.channel.id == selectChannel && !reaction.message.member.roles.cache.has('866784910147977216')){
 
-            } else {
-                return;
-            }
-        });
-
-
-        client.on('messageReactionRemove', async (reaction, user) => {
-            if(reaction.message.partial) await reaction.message.fetch();
-            if(reaction.partial) await reaction.fetch();
-            if(user.bot) return;
-            if(!reaction.message.guild) return;
-
-            if(reaction.message.channel.id == selectChannel){
+                let userId = await reaction.message.guild.members.cache.get(user.id);
+                let militaryTypeName;
 
                 switch(reaction.emoji.name){
                     case armyEmoji:
+                        userId.roles.add(applicant);
+                        militaryTypeName = "Himthitki"
+                        tagString = "<@256762652565569536>"
+                        //            becon
+                        break;
                     case dragoonEmoji:
+                        userId.roles.add(applicant);
+                        militaryTypeName = "Dragoon cavalry"
+                        tagString = "<@256762652565569536>"
+                        //            becon
+                        break;
                     case guardEmoji:
-                        await reaction.message.guild.members.cache.get(user.id).roles.remove(recruit);
+                        userId.roles.add(applicant);
+                        militaryTypeName = "Town guard"
+                        tagString = "<@256762652565569536>"
+                        //            becon
                         break;
                     default:
-                        console.error("there is no emoji like that in jobs");
+                        console.error("there is no emoji like that in military selection");
+                        return;
                 }
 
+                let embed = new Discord.MessageEmbed()
+                    .setColor('#e42643')
+                    .setTitle('Military')
+                    .setDescription('New applicant for '+ militaryTypeName +' <@' + userId + '>');
+
+                client.channels.cache.get(botCommunicationChannel).send(embed);
+                client.channels.cache.get(botCommunicationChannel).send(tagString);
+                    
             } else {
                 return;
             }
+            
         });
-
-
-
     }
 }
